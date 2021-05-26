c42732badf95507496c21d21414b673a8b0312cb
TravisWongX <travis.x.wong@gmail.com>
2021-05-26 16:39:44 +0800

    for loop

---
+++ chapter 4/pizzas.py
@@ -0,0 +1,8 @@
+pizzas = ['pineapple', 'pepperoni', 'ham', 'cheese']
+animals = ['giraffe', 'gorilla', 'buffalo']
+
+for pizza in pizzas:
+    for animal in animals:
+        print(f"{animal} loves {pizza} pizza")
+
+print("All animals really love pizza!")
