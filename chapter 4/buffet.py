cdb13f42a45bdcb5afe83f604279734db20b732e
TravisWongX <travis.x.wong@gmail.com>
2021-05-27 17:17:55 +0800

    tuple

---
+++ chapter 4/buffet.py
@@ -0,0 +1,6 @@
+buffet = ('sugar', 'fish', 'steak', 'pizza', 'ice cream')
+for item in buffet:
+    print(item)
+print(buffet)
+buffet = ('sugar', 'apple', 'steak', 'noodle', 'ice cream')
+print(buffet)
---
+++ chapter 4/tuples.py
@@ -0,0 +1,3 @@
+dimension = (200, 50)
+my_t = (1,)
+print(my_t[0])
