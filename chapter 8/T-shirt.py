0689a5495cda4c30963706c0e5d943d4d73f7cdb
travis <travis.x.wong@gmail.com>
2021-06-07 22:32:21 +0800

    function arguments

---
+++ chapter 8/T-shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(size='L', message='hello world'):
+  print(f"size: {size}, message: {message}")
+
+make_shirt('M', 'dog')
+make_shirt(message='cat', size='L')
+make_shirt('S')
+make_shirt(message='I love Python')
