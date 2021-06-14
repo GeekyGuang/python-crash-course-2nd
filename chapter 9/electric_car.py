51d243bc51e7d7d3ca12d0bbcca5068a0ac2fee8
travis <travis.x.wong@gmail.com>
2021-06-14 20:09:04 +0800

    inheritance

---
+++ chapter 9/electric_car.py
@@ -0,0 +1,55 @@
+class Car:
+  """A simple attempt to represent a car."""
+
+  def __init__(self, make, model, year):
+    self.make = make
+    self.model = model
+    self.year = year
+    self.odometer_reading = 0
+
+  def get_descriptive_name(self):
+    long_name = f"{self.year} {self.make} {self.model}"
+    return long_name.title()
+
+  def read_odometer(self):
+    print(f"This car has {self.odometer_reading} miles on it.")
+
+  def update_odometer(self, mileage):
+    if mileage >= self.odometer_reading:
+      self.odometer_reading = mileage
+    else:
+      print("You can't roll back an odometer!")
+  
+  def increment_odomenter(self, miles):
+    self.odometer_reading += miles
+
+class Battery:
+  """A Simple attempt to model a battery for an electric car."""
+  
+  def __init__(self, battery_size=75):
+    """Initialize the battery's attributes."""
+    self.battery_size = battery_size
+  
+  def describe_battery(self):
+    """Print a statement describing the battery size."""
+    print(f"This car has a {self.battery_size}-kWh battery.")
+
+# 继承
+class ElectricCar(Car):
+  """Reresent aspects of a car, specific to electric vehicles."""
+
+  def __init__(self, make, model, year):
+    """Initialize attributes of the parent class."""
+    super().__init__(make, model,year) # 继承父class属性
+    self.battery = Battery()  # instance as attribute
+  
+  # override父元素方法
+  def fill_gas_tank(self):
+    """Electric cars don't have gas tanks."""
+    print("This car doesn't need a gas tank!")
+
+  
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()  # 调用属性上的方法
+my_tesla.fill_gas_tank()
